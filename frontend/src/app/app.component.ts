be9205d3958ce0891ddae768a763e039931c2b39
vinothvarun94 <vinothvarun94@gmail.com>
2021-02-24 17:59:42 +0530

    good commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frakton';
-  newVar="MAIN";
-  Sample="MAIN";
+  newVar="MAIN GOO CHNAGE";
+  Sample="MAIN GOOD CHANGES";
 }
