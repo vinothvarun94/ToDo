bffd14b370a7fbc9c771dee52bbdcd54d6aa5813
vinothvarun94 <vinothvarun94@gmail.com>
2021-02-24 17:13:21 +0530

    2 nd commit from branch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frakton';
-  newVar="Main Content Changed";
+  newVar="Sample Change";
+  Sample="C:\Program Files\SourceGear\Common\DiffMerge"
 }
