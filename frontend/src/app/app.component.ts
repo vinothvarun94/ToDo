2f2ee6986cbb9d6e095ed99a120e60ce15cd52fe
vinothvarun94 <vinothvarun94@gmail.com>
2021-02-24 17:33:28 +0530

    2 branch commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,11 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frakton';
-<<<<<<< HEAD
-  newVar="DIFF MAIN";
-  Sample="DIFF MAIN";
-=======
-  newVar="Sample Change";
-  Sample="C:\Program Files\SourceGear\Common\DiffMerge"
->>>>>>> main
+  newVar="SUB BRANCH";
+  Sample="SUB BRANCH";
 }
